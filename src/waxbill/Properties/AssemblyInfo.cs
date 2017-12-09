7bbe2f75bbdd0d93fa12d7451bed7d2d31f3ab4f
asmrobot <asmrobot@hotmail.com>
2017-12-09 16:19:13 +0800

    add targets

--- src/waxbill/Properties/AssemblyInfo.cs
+++ src/waxbill/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("waxbill")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("sample server lib")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("waxbill")]
