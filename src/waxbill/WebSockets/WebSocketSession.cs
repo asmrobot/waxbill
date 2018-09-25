b2de024a673e00cdb9c9a2034fc7982a6c9123d7
asmrobot <asmrobot@hotmail.com>
2018-09-25 17:46:08 +0800

    new project type

--- src/waxbill/WebSockets/WebSocketSession.cs
+++ src/waxbill/WebSockets/WebSocketSession.cs
@@ -7,7 +7,7 @@ using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
-using waxbill.Libuv;
+using ZTImage.Libuv;
 using waxbill.Packets;
 using waxbill.Sessions;
 using waxbill.Utils;
